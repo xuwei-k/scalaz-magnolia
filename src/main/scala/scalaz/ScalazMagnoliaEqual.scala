e1cab38a3fb7907cbbfb17e76caf8710bf8b2e4e
xuwei-k <6b656e6a69@gmail.com>
2020-02-15 09:34:16 +0000

    Update scalafmt to 2.4.0

--- src/main/scala/scalaz/ScalazMagnoliaEqual.scala
+++ src/main/scala/scalaz/ScalazMagnoliaEqual.scala
@@ -6,17 +6,11 @@ object ScalazMagnoliaEqual {
   type Typeclass[A] = scalaz.Equal[A]
 
   def combine[A](ctx: CaseClass[Typeclass, A]): Typeclass[A] =
-    Equal.equal[A] { (a, b) =>
-      ctx.parameters.forall { p =>
-        p.typeclass.equal(p.dereference(a), p.dereference(b))
-      }
-    }
+    Equal.equal[A] { (a, b) => ctx.parameters.forall { p => p.typeclass.equal(p.dereference(a), p.dereference(b)) } }
 
   def dispatch[A](ctx: SealedTrait[Typeclass, A]): Typeclass[A] =
     Equal.equal[A] { (a, b) =>
-      ctx.dispatch(a) { sub =>
-        sub.cast.isDefinedAt(b) && sub.typeclass.equal(sub.cast(a), sub.cast(b))
-      }
+      ctx.dispatch(a) { sub => sub.cast.isDefinedAt(b) && sub.typeclass.equal(sub.cast(a), sub.cast(b)) }
     }
 
   implicit def scalazEqualAutoInstance[A]: Typeclass[A] =
